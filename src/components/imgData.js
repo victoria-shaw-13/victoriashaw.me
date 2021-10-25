bd1ab40204f09706372d8d0a12628063b509eff9
Victoria Shaw <victoriashaw@Victorias-MacBook-Air.local>
2021-10-24 22:09:08 -0400

    not visible

--- src/components/imgData.js
+++ src/components/imgData.js
@@ -1,48 +1,48 @@
 export const imgData= [
 {
-    image: 'images/art portfolio/IMG_1.jpg'
+    image: '/images/art portfolio/IMG_1.jpg'
 },
 {
-    image: 'images/art portfolio/IMG_2.JPG'
+    image: '/images/art portfolio/IMG_2.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_3.JPG'
+    image: '/images/art portfolio/IMG_3.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_4.JPG'
+    image: '/images/art portfolio/IMG_4.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_5.JPG'
+    image: '/images/art portfolio/IMG_5.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_6.JPG'
+    image: '/images/art portfolio/IMG_6.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_7.jpg'
+    image: '/images/art portfolio/IMG_7.jpg'
 },
 {
-    image: 'images/art portfolio/IMG_8.JPG'
+    image: '/images/art portfolio/IMG_8.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_9.JPG'
+    image: '/images/art portfolio/IMG_9.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_10.JPG'
+    image: '/images/art portfolio/IMG_10.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_11.JPG'
+    image: '/images/art portfolio/IMG_11.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_12.jpg'
+    image: '/images/art portfolio/IMG_12.jpg'
 },
 {
-    image: 'images/art portfolio/IMG_13.JPG'
+    image: '/images/art portfolio/IMG_13.JPG'
 },
 {
-    image: 'images/art portfolio/IMG_14.jpg'
+    image: '/images/art portfolio/IMG_14.jpg'
 },
 {
-    image: 'images/art portfolio/IMG_15.JPG'
+    image: '/images/art portfolio/IMG_15.JPG'
 }
 
 
